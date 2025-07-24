185a5105336e6da62464e09739a9686bd792157f
dehez <olivier.dehez1203@gmail.com>
2025-07-24 18:00:21 +0200

    Création de la BDD, de l'entité USER et mise en place du bundle odolbeau/phone-number-bundle

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use libphonenumber\PhoneNumberUtil;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture implements FixtureGroupInterface
+{
+    public static function getGroups(): array
+    {
+        return ['user'];
+    }
+
+    public function __construct(protected UserPasswordHasherInterface $passwordHasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $phoneNumberUtil = PhoneNumberUtil::getInstance(); // Instance de PhoneNumberUtil
+
+        $admin = new User();
+        $hash = $this->passwordHasher->hashPassword($admin, "password");
+
+        // Génère un numéro de téléphone pour l'admin
+        $adminRawPhoneNumber = $faker->mobileNumber();
+        $adminPhoneNumberObject = $phoneNumberUtil->parse($adminRawPhoneNumber, 'FR');
+
+        $admin->setFirstname("Admin")
+            ->setLastname("Admin")
+            ->setRoles(['ROLE_ADMIN'])
+            ->setAdress($faker->streetAddress())
+            ->setPostalCode($faker->postcode())
+            ->setCity($faker->city())
+            // ->setPhone($faker->phoneNumber())
+            ->setPhone($adminPhoneNumberObject)
+            ->setEmail("admin@gmail.com")
+            ->setPassword($hash);
+
+        $manager->persist($admin);
+
+        $users = [];
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+
+            // Génère un numéro de téléphone différent pour chaque utilisateur
+            $rawPhoneNumber = $faker->mobileNumber();
+            $phoneNumberObject = $phoneNumberUtil->parse($rawPhoneNumber, 'FR');
+
+            $user->setEmail("user$u@gmail.com")
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName())
+                ->setAdress($faker->streetAddress())
+                ->setPostalCode($faker->postcode())
+                ->setCity($faker->city())
+                // ->setPhone($faker->phoneNumber())
+                ->setPhone($phoneNumberObject)
+                ->setPassword($hash);
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use libphonenumber\PhoneNumber;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Misd\PhoneNumberBundle\Validator\Constraints\PhoneNumber as AssertPhoneNumber;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adress = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $postalCode = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(type: 'phone_number')]
+    #[AssertPhoneNumber(defaultRegion: 'FR')]
+    private ?PhoneNumber $phone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+        // @deprecated, to be removed when upgrading to Symfony 8
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): static
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(string $postalCode): static
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPhone(): ?PhoneNumber
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?PhoneNumber $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
