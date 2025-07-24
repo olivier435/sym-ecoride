5e90e75b55267184854c98a9f4727cb9dbc391de
dehez <olivier.dehez1203@gmail.com>
2025-07-24 12:09:10 +0200

    Mise en place des assets et du HomeController

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,24 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap';
 
+// Ferme les messages d'alerte après 5 secondes
+const closeAlertMessage = () => {
+    const alert = document.querySelector('.alert');
+    if (alert) {
+        setTimeout(() => {
+            alert.classList.add('fade-out'); // Ajoute une classe CSS pour la transition
+            setTimeout(() => alert.remove(), 1000); // Retire l'alerte après la transition
+        }, 4000);
+    }
+}
+
+const initPage = () => {
+    closeAlertMessage();
+}
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+document.addEventListener('load', initPage);
+document.addEventListener('turbo:load', initPage);
