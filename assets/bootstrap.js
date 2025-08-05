566c38a306574a8b860f934258aff388f870b7db
dehez <olivier.dehez1203@gmail.com>
2025-08-05 08:30:24 +0200

    Correction orthographique de l'import dans bootstrap.js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import ModelLoaderController from './controllers/model_loader_controller.js';
 import RegistrationFormatterController from './controllers/registration_formatter_controller.js';
-import AdressFormatterController from './controllers/address_formatter_controller.js';
+import AddressFormatterController from './controllers/address_formatter_controller.js';
 import SeatsCounterController from './controllers/seats_counter_controller.js';
 import PriceController from './controllers/price_controller.js';
 
@@ -9,6 +9,6 @@ const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 app.register('model-loader', ModelLoaderController);
 app.register('registration-formatter', RegistrationFormatterController);
-app.register('address-formatter', AdressFormatterController);
+app.register('address-formatter', AddressFormatterController);
 app.register('seats-counter', SeatsCounterController);
 app.register('price', PriceController);
